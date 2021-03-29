4c2a8b45100484dd11c47957a602d4eb1fc106f4
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2021-03-28 21:54:20 -0300

    fix project names

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import DashboardMiniApp from './src/App';
+import ProductsMiniApp from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => DashboardMiniApp);
+AppRegistry.registerComponent(appName, () => ProductsMiniApp);
