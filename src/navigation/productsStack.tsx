b44cbd21ec75fd40ad537d0e41b1de3c3b7f5dae
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2021-03-28 22:10:09 -0300

    simplify the stack

--- src/navigation/productsStack.tsx
+++ src/navigation/productsStack.tsx
@@ -9,22 +9,16 @@ import {NavigationContainer} from '@react-navigation/native';
 
 const Stack = createStackNavigator<MainStackParams>();
 
-function renderStack(): JSX.Element {
-  return (
-    <Stack.Navigator initialRouteName={ScreenNames.ProductsScreen}>
-      <Stack.Screen name={ScreenNames.ProductsScreen} component={Products} />
-      <Stack.Screen
-        name={ScreenNames.ProductsDetailScreen}
-        component={ProductsDetails}
-      />
-    </Stack.Navigator>
-  );
-}
-
 export function ProductsStack() {
   return (
     <NavigationContainer independent={true}>
-      {renderStack()}
+      <Stack.Navigator initialRouteName={ScreenNames.ProductsScreen}>
+        <Stack.Screen name={ScreenNames.ProductsScreen} component={Products} />
+        <Stack.Screen
+          name={ScreenNames.ProductsDetailScreen}
+          component={ProductsDetails}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
